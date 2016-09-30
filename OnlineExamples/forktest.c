60d0d6f974e2e354119d1191b3cb2911c84e3913
polston <polston@gmail.com>
2016-09-30 23:57:09 +0000

    Add all the relevant code from youtube tutorial.

--- OnlineExamples/forktest.c
+++ OnlineExamples/forktest.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
+void doSomeWork(char *name)
+{
+    const int NUM_TIMES = 5;
+    for(int i = 0; i < NUM_TIMES; i++)
+    {
+        sleep(rand() % 4);
+        printf("Done, pass %d for %s\n", i, name);
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +20,7 @@ int main(int argc, char *argv[])
     
     //create child
     pid_t pid = fork();
+    srand((int)pid);
     printf("Fork returned: %d\n", (int)pid);
     
     
@@ -19,10 +31,18 @@ int main(int argc, char *argv[])
     else if(pid == 0) //if inside child
     {
         printf("I am the child with pid: %d\n", (int)getpid());
-        exit(0);
+        doSomeWork("Child");
+        printf("Child exiting\n");
+        exit(42);
     }
     //we must be the parent, because the child exited
-    printf("I am the parent\n");
+    printf("I am the parent, waiting for child to end.\n");
+    doSomeWork("Parent");
+    int status = 0;
+    pid_t childpid = wait(&status);
+    printf("Parent knows child %d finished with status %d.\n", (int)childpid, status);
+    int childReturnValue = WEXITSTATUS(status);
+    printf("Return value was %d\n", childReturnValue);
     
     return 0;
 }
