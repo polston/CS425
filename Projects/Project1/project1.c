7ae0e300a4bd4991e99ec54b0ed689c199b281ce
polston <polston@gmail.com>
2016-10-05 21:01:18 +0000

    History partially works, but... updates the history list in two seperate instances of itself? Hard to explain, watch output...

--- Projects/Project1/project1.c
+++ Projects/Project1/project1.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 #define MAX_LINE 90
+#define HISTORY_SIZE 10
 
-void setup(char* strArr[], char str[])
+//parses user input into character pointer array to be passed into execvp
+void setup(char* args[], char in[])
 {
-	char **next = strArr;
-	char *t = strtok(str, " ");
+	char **next = args;
+	char *t = strtok(in, " ");
 		int count = 0;
 		
 		while (t != NULL)
@@ -20,9 +23,28 @@ void setup(char* strArr[], char str[])
 	    *next = NULL;
 }
 
+void addHist(char** hist, char in[], int histCount)
+{
+	char* tempStr = (char*)malloc(strlen(in)+1);
+	strcpy(tempStr, in);
+	int tempHistCount = histCount % HISTORY_SIZE;
+	strcpy(hist[tempHistCount], tempStr);
+}
+
+
+
 int main(void)
 {
 	char *args[MAX_LINE]; // command line arguments
+	char **hist;
+	int histCount = 0;
+	hist = malloc(HISTORY_SIZE * sizeof(char *));
+	
+	for(int i = 0; i < HISTORY_SIZE; i++)
+	{
+		hist[i] = malloc(HISTORY_SIZE * sizeof(char));
+    	hist[i][0] = '\0';
+	}
 	
 	char input[MAX_LINE];
 	int should_run = 1;
@@ -32,14 +54,17 @@ int main(void)
 
 	while(should_run)
 	{
-		fflush(stdout);
+		
 		printf("COMMAND->");
+		fflush(stdout);
 		
 		/*After reading user input, the steps are:
 		 *(1) for a child process using fork();
 		 *(2) the child process will invoke execvp();
 		 *(3) if command included &, parent will invoke wait()
 		*/
+		
+		//gets user input
 		fgets(input, sizeof(input), stdin);
 		int len;
 		len = strlen(input);
@@ -47,35 +72,28 @@ int main(void)
 		    input[len-1] = '\0';
 		}
 		
-		printf("test %s\n", input);
-		
+		//printf("test %s\n", input);
 		//read user input
 		//Put strings into the array
-		/*
-		char *t = strtok(input, " ");
-		int count = 0;
 		
-		while (t != NULL)
-	    {
-	        //printf("t: %s\n", t);
-	        *next++ = t;
-	        //strcpy(args[count], t);
-	        t = strtok(NULL, " ");
-	        //printf("strArr[count]: %s\n", args[count]);
-	        //count++;
-	    }
-	    *next = NULL;
-	    */
-	    
+		//addHist(hist, input, histCount);
+		addHist(hist, input, histCount);
+		histCount++;
 	    setup(args, input);
 	    
-	    puts("Checking:");
+	    /*
         for (char **next = args; *next != 0; next++)
-            puts(*next);
-	    
+            printf("Checking: %s\n", *next);
+    	*/
+
+        for (char **next = hist; *next != 0; next++)
+            printf("History: %s\n", *next);
+            
+	    printf("HistCount: %d\n", histCount);
+	    //printf("-----------------------------------------------------\n");
 		pid = fork();
 		
-
+		//printf("-----------------------------------------------------\n");
 		if(pid < 0)
 		{
 			perror("Fork failed.");
@@ -85,10 +103,11 @@ int main(void)
 			printf("in child\n");
 			//char usrInput[MAX_LINE/2+1];
 			execvp(args[0], args);
+			
 			//sleep(5);
 		}
 		
-		sleep(1);
+		//sleep(1);
 		//in parent
 		printf("in parent\n");
 		wait(NULL);
